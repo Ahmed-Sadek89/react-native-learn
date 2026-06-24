6d5262f9b457ccd1fa0c3e9ed5e766ceaace541d
Ahmed Sadek <ahmeamohamed161@gmail.com>
2026-06-24 20:06:47 +0300

    first installation

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View>
+        <Text>Ahmed Sadek</Text>
+        <Text>amr Sadek</Text>
+    </View>
+  )
+}
+
+export default index
