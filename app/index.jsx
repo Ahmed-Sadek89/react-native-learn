02f3e5270c21e3342450bd0378e083d6cd666224
Ahmed Sadek <ahmeamohamed161@gmail.com>
2026-06-28 15:43:08 +0300

    feat: my first touch

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,45 @@
-import { Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import Sadek from '../assets/images/ahmed-sadek.jpeg';
 
 const index = () => {
   return (
-    <View>
-        <Text>Ahmed Sadek</Text>
-        <Text>amr Sadek</Text>
+    <View style={[styles.container]}>
+      <Image 
+        source={Sadek}
+        style={[styles.mainImage]}
+      />
+      <Text style={styles.title}>Ahmed Sadek</Text>
+      <Text style={styles.subtitle}>Frontend developer & Instructor</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    rowGap: 20
+  },
+  mainImage: {
+    width:300,
+    height:300,
+    borderRadius: 20,
+  },  
+  title: {
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    backgroundColor: "#333",
+    color: "#fff",
+    borderRadius: 20,
+    fontWeight: "bold",
+    fontSize: 30
+  },
+  subtitle: {
+    fontSize: 20,
+    color: "#333",
+    textDecorationLine: "underline"
+  }
+})
 
 export default index
